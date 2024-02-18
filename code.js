ced347589af691a7fb917a58ced54acb2cae735d
JPbonaldo <159311815+JPbonaldo@users.noreply.github.com>
2024-02-18 04:55:26 +0000

    second commit

--- code.js
+++ code.js
@@ -1,5 +1,7 @@
 window.onload = () => {
-    document.querySelector('#the-excuse').innerHTML = generateExcuse();
+    document.querySelector('#btn').addEventListener("click", () => {
+        document.querySelector('#the-excuse').innerHTML = generateExcuse();
+});
 };
 
 let generateExcuse = () => {
@@ -7,12 +9,12 @@ let generateExcuse = () => {
     let who = ['My fish', 'I', 'My llama','My Uber driver'];
     let action = ['drowned', 'ate', 'threw up','got stuck', 'peed on', 'crushed', 'broke'];
     let what = ['itself', 'cat food instead of tuna', 'my homework', 'the keys', 'the car'];
-    let when = ['earlier', 'last night', 'yesterday', 'on the way here', 'before the class', 'right on time', 'when I finished', 'during my lunch',];
+    let when = ['earlier', 'last night', 'yesterday', 'on the way here', 'before the class', 'right on time', 'when I finished', 'during my lunch'];
 
-    let whoIndex = 0;
-    let actionIndex = 0;
-    let whatIndex = 0;
-    let whenIndex = 0;
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
 
     return who[whoIndex] + ' ' + action[actionIndex] + ' ' + when[whenIndex] + ' ' + what[whatIndex];
 };
